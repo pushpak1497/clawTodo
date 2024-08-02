564069540365bcc7f8a850f3e7a0bfb2a1485ed8
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-08-02 22:03:37 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -11,8 +11,6 @@ const generateAccessAndRefreshTokens = async (userId) => {
     const user = await User.findById(userId);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
-    user.refreshToken = refreshToken;
-    await user.save({ validateBeforeSave: false });
     return { accessToken, refreshToken };
   } catch (error) {
     throw new ApiError(500, "something went wrong while generating tokens");
